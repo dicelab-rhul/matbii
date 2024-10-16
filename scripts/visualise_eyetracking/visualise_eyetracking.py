24fc993cab64b615267816e9312af784d0b3b3d6
Benedict Wilkins <benrjw@gmail.com>
2024-10-16 15:07:01 +0100

    minor updates to eyetracking visualisation

--- scripts/visualise_eyetracking/visualise_eyetracking.py
+++ scripts/visualise_eyetracking/visualise_eyetracking.py
@@ -1,4 +1,12 @@
-"""Eyetracking visualisation. This will create an avatar that displays the mouse and gaze location of the user. It may be used for debugging purposes, or to choose suitable parameters for a new eyetracker."""
+"""Eyetracking visualisation. 
+
+This will create an avatar that displays the mouse and gaze location of the user. It may be used for debugging purposes, or to choose suitable parameters for a new eyetracker.
+
+The eyetracking position is represented as a blue dot, the mouse position is a red dot. 
+The blue dot will change its size depending on fixation/saccade. Small for fixation, large for saccade.
+
+If the eyetracker fails to load you will get a message in the console saying so (its probably a URI issue, make sure you get it from your eyetracker manager and the eyetracker has been calibrated and is on.)
+"""
 
 from icua import MultiTaskEnvironment
 from icua.agent import Avatar as _Avatar, Actuator as _Actuator, attempt, observe
@@ -13,17 +21,20 @@ from star_ray.utils import _LOGGER
 _LOGGER.setLevel("WARNING")
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--uri", type=str)
-argparser.add_argument("--sdk", type=str, default="tobii")
+argparser.add_argument("--uri", type=str, help="The uri (address) of the eye tracker device, e.g. `tet-tcp://169.254.126.68`")
+argparser.add_argument("--sdk", type=str, default="tobii", help="The sdk to use, defaults to `tobii`.")
+argparser.add_argument("--ma", type=int, default=10, help="Moving average n")
+argparser.add_argument("--vt", type=float, default=0.5, help="velocity threshold (normalised screen space)")
+argparser.add_argument("--fullscreen", "-f", action="store_true")
 
 args = argparser.parse_args()
 
 window_config = WindowConfiguration(
     width=640,
     height=640,
+    fullscreen=args.fullscreen,
     title="Eyetracking Visualisation",
 )
-
 
 class Actuator(_Actuator):
     """Actuator base class, see `EyeActuator` and `MouseActuator` for examples."""
@@ -123,7 +134,7 @@ avatar = Avatar(
 )
 
 eyetracking_config = EyetrackingConfiguration(
-    uri=args.uri, sdk=args.sdk, moving_average_n=10, velocity_threshold=0.5
+    uri=args.uri, sdk=args.sdk, moving_average_n=args.ma, velocity_threshold=args.vt, enabled=True
 )
 eyetracking_sensor = EyetrackingConfiguration.new_eyetracking_sensor(eyetracking_config)
 
@@ -136,7 +147,16 @@ else:
 
 
 TASK_ID = "visualise_eyetracking"
-env = MultiTaskEnvironment(avatar=avatar)
+if window_config.fullscreen:
+    svg_size = (1920, 1080) # this is monitor specific, change it if things break! TODO use pygame to get the window size...
+else:
+    svg_size=(window_config.width, window_config.height)
+
+print(svg_size)
+env = MultiTaskEnvironment(avatar=avatar,  
+        svg_size=svg_size, # resize the svg to fit whatever the window size is...
+        svg_position=(0,0)
+    )
 env.add_task(
     name=TASK_ID,
     path=["./"],
